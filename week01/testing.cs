33a4db87e3982d57d84b2d0ea91e3dc1769f6ca6
stephenmoll <stephenmollandco@mail.com>
2023-11-08 20:47:16 +0200

    uploading stuff I forgot to

---
+++ week01/testing.cs
@@ -0,0 +1,18 @@
+public static class test {    
+    public static void test () 
+    {
+        var strings = new List<string> { Bob, John, Rex };
+        bool FindBob(List strings)
+        {
+        foreach (var name in nameList)
+        {
+            if (name == "Bob")
+            {
+            return true;
+            }
+        }
+        
+        return false;
+        }
+    }
+}    
