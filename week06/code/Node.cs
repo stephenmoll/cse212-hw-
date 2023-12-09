4c915612c6aa8deb5122f6e71b23b0edf2b919e2
stephenmoll <stephenmollandco@mail.com>
2023-12-09 23:17:04 +0200

    1-4

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -14,23 +14,47 @@ public class Node {
                 Left = new Node(value);
             else
                 Left.Insert(value);
-        }
-        else {
+        }     
+        else if (value > Data) {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
+        else {
+        }    
     }
 
     public bool Contains(int value) {
         // TODO Start Problem 2
+        if (value == Data || Left is not null && Left.Contains(value) || Right is not null && Right.Contains(value)) {
+            
+            return true;
+        }
         return false;
     }
+    
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+
+        int leftSubtreeHeight = 0;
+        int rightSubtreeHeight = 0;
+        
+        if (Left is not null && Right is not null){
+        }
+
+        if (Left is not null) {
+            leftSubtreeHeight = Left.GetHeight();
+        }
+
+        if (Right is not null) {
+            rightSubtreeHeight = Right.GetHeight();
+        }
+
+        var height = 1 + Math.Max(leftSubtreeHeight, rightSubtreeHeight);
+        
+        return height;
     }
 }
